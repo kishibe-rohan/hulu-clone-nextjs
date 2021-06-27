98076790fbc57a58d10ae8329a878bc277b881b6
kishibe-rohan <shahnawaz.21810589@viit.ac.in>
2021-06-27 10:23:31 +0530

    added Nav component

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,25 @@
+import requests from "../utils/requests";
+import { useRouter } from "next/router";
+
 function Nav() {
-  return <div></div>;
+  const router = useRouter();
+  return (
+    <nav className="relative">
+      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
+        {Object.entries(requests).map(([key, { title, url }]) => (
+          <h2
+            key={key}
+            onClick={() => {
+              router.push(`/?genre=${key}`);
+            }}
+            className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-110 hover:text-blue-500 "
+          >
+            {title}
+          </h2>
+        ))}
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
